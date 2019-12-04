1ca8a57a60e107df82fc31cde87643296e21dfea
Priyank Shah <prshah@apttus.com>
2019-12-04 23:31:24 +0530

    Initial Commit

---
+++ src/test/java/webdriverMaven/loginTest.java
@@ -0,0 +1,43 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginTest {
+
+	public static WebDriver driver;
+
+	
+	@BeforeSuite
+	public void setup() {
+
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+
+	}
+
+	@Test
+	public void doLogin() {
+
+		driver.get("http://google.com");
+		driver.findElement(By.xpath("/html/body/div/div[4]/form/div[2]/div[1]/div[1]/div/div[2]/input"))
+				.sendKeys("Way2Automation");
+		driver.findElement(By.xpath("/html/body/div/div[4]/form/div[2]/div[1]/div[3]/center/input[1]")).click();
+
+		
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+		
+	}
+
+}
