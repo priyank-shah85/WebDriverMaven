8130f9e49a1d3425e29c774b55b52baa8bd30528
Priyank Shah <prshah@apttus.com>
2019-12-04 23:39:38 +0530

    Committing the changes made to LoginTest

--- src/test/java/webdriverMaven/loginTest.java
+++ src/test/java/webdriverMaven/loginTest.java
@@ -29,7 +29,7 @@ public class loginTest {
 		driver.findElement(By.xpath("/html/body/div/div[4]/form/div[2]/div[1]/div[1]/div/div[2]/input"))
 				.sendKeys("Way2Automation");
 		driver.findElement(By.xpath("/html/body/div/div[4]/form/div[2]/div[1]/div[3]/center/input[1]")).click();
-
+		driver.findElement(By.xpath("/html/body/div[7]/div[3]/div[10]/div[1]/div[2]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[1]/a/h3/span")).click();
 		
 	}
 	
